2a6dd4bea1ee6b7db175ca39066e3952c5604ac6
brilliahib <brilliahib21@gmail.com>
2025-04-17 19:29:22 +0700

    fix: typo relationship on screening and pre test

--- app/Http/Controllers/UserHistoryScreeningController.php
+++ app/Http/Controllers/UserHistoryScreeningController.php
@@ -23,7 +23,7 @@ class UserHistoryScreeningController extends Controller
 
     public function getAllHistory()
     {
-        $histories = UserHistoryScreening::with(['screenings', 'user'])
+        $histories = UserHistoryScreening::with(['screening', 'user'])
             ->orderBy('created_at', 'desc')
             ->get();
 
